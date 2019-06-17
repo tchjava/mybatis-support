1f1c263a810f5967f62732cb65273149dcb223e9
wengzhongjie <1310120194@qq.com>
2019-06-17 21:01:41 +0800

    #sync supplier_info表

--- src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/SupplierInfo.java
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/SupplierInfo.java
@@ -16,30 +16,76 @@ import java.util.Date;
  * </p>
  *
  * @author Gaby
- * @since 2019-06-16
+ * @since 2019-06-17
  */
 @TableName("supplier_info")
 public class SupplierInfo implements Serializable, IEntity {
 
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
+    /**
+     * 店铺名称
+     */
     private String nickname;
+    /**
+     * 微信ID
+     */
     private String openId;
+    /**
+     * 店铺ID
+     */
     private String shopUri;
+    /**
+     * 联系电话
+     */
     private String phone;
+    /**
+     * 头像
+     */
     private String headimg;
+    /**
+     * 入库时间
+     */
     private Date intime;
+    /**
+     * 店铺等级
+     */
     private Integer level;
+    /**
+     * 店铺积分
+     */
     private Integer score;
+    /**
+     * 店铺二维码
+     */
     private String qrcode;
+    /**
+     * 店铺评分
+     */
     private BigDecimal rate;
+    /**
+     * 身份认证
+     */
     @TableField("id_card")
     private String idCard;
+    /**
+     * 食品安全认证
+     */
     @TableField("foot_safe")
     private Integer footSafe;
+    /**
+     * 磁盘大小
+     */
     private Long capacity;
+    /**
+     * 用户标识
+     */
     @TableField("user_account_id")
     private Long userAccountId;
+    /**
+     * 平台标识
+     */
+    private Long appId;
 
     public Long getId() {
         return id;
@@ -190,39 +236,95 @@ public class SupplierInfo implements Serializable, IEntity {
         return this;
     }
 
+    public Long getAppId() {
+        return appId;
+    }
+
+    public SupplierInfo setAppId(Long appId) {
+        this.appId = appId;
+        return this;
+    }
+
     /**
      * 表名
      */
     public static final String TABLE_NAME = "supplier_info";
     public static final String ID = "id";
 
+    /**
+     * 店铺名称
+     */
     public static final String NICKNAME = "nickname";
 
+    /**
+     * 微信ID
+     */
     public static final String OPENID = "openId";
 
+    /**
+     * 店铺ID
+     */
     public static final String SHOPURI = "shopUri";
 
+    /**
+     * 联系电话
+     */
     public static final String PHONE = "phone";
 
+    /**
+     * 头像
+     */
     public static final String HEADIMG = "headimg";
 
+    /**
+     * 入库时间
+     */
     public static final String INTIME = "intime";
 
+    /**
+     * 店铺等级
+     */
     public static final String LEVEL = "level";
 
+    /**
+     * 店铺积分
+     */
     public static final String SCORE = "score";
 
+    /**
+     * 店铺二维码
+     */
     public static final String QRCODE = "qrcode";
 
+    /**
+     * 店铺评分
+     */
     public static final String RATE = "rate";
 
+    /**
+     * 身份认证
+     */
     public static final String ID_CARD = "id_card";
 
+    /**
+     * 食品安全认证
+     */
     public static final String FOOT_SAFE = "foot_safe";
 
+    /**
+     * 磁盘大小
+     */
     public static final String CAPACITY = "capacity";
 
+    /**
+     * 用户标识
+     */
     public static final String USER_ACCOUNT_ID = "user_account_id";
+
+    /**
+     * 平台标识
+     */
+    public static final String APPID = "appId";
 
     @Override
     public SupplierInfo pkVal(Serializable val) {
