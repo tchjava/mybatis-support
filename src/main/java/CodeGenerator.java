289943a718f7cc11c7ab68dd6f2f5a46b9285f35
wengzhongjie <1310120194@qq.com>
2019-11-29 15:20:59 +0800

    #Mod  调整模版文件

--- src/main/java/CodeGenerator.java
+++ src/main/java/CodeGenerator.java
@@ -24,23 +24,25 @@ public class CodeGenerator {
      * </p>
      */
     public static int scanner() {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder help = new StringBuilder();
-        help.append(" ！！代码生成, 输入 0 表示使用 Velocity 引擎 ！！");
-        help.append("\n对照表：");
-        help.append("\n0 = Velocity 引擎");
-        help.append("\n1 = Freemarker 引擎");
-        help.append("\n请输入：");
-        System.out.println(help.toString());
-        int slt = 0;
-        // 现在有输入数据
-        if (scanner.hasNext()) {
-            String ipt = scanner.next();
-            if ("1".equals(ipt)) {
-                slt = 1;
-            }
-        }
-        return slt;
+//        Scanner scanner = new Scanner(System.in);
+//        StringBuilder help = new StringBuilder();
+//        help.append(" ！！代码生成, 输入 0 表示使用 Velocity 引擎 ！！");
+//        help.append("\n对照表：");
+//        help.append("\n0 = Velocity 引擎");
+//        help.append("\n1 = Freemarker 引擎");
+//        help.append("\n请输入：");
+//        System.out.println(help.toString());
+//        int slt = 0;
+//        // 现在有输入数据
+//        if (scanner.hasNext()) {
+//            String ipt = scanner.next();
+//            if ("1".equals(ipt)) {
+//                slt = 1;
+//            }
+//        }
+//        return slt;
+        //写死 Velocity 引擎
+        return 0;
     }
 
     public static void main(String[] args) {
