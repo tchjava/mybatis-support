5d5322e29e4a2a827c37ac0e5f7760552d689ef6
wengzhongjie <534139685@qq.com>
2020-01-06 14:55:30 +0800

    #Mod sync 任务开发信息表

--- src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/TaskCycleInfo.java
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/TaskCycleInfo.java
@@ -1,13 +1,17 @@
 package com.tjlou.mybatis.auto.mysql.sps.entity;
 
+            import com.baomidou.mybatisplus.enums.IdType;
+        import java.util.Date;
+        import com.baomidou.mybatisplus.annotations.TableId;
+        import com.baomidou.mybatisplus.annotations.TableField;
+        import com.baomidou.mybatisplus.annotations.TableId;
+        import com.baomidou.mybatisplus.enums.IdType;
+        import com.baomidou.mybatisplus.annotations.TableName;
+        import java.io.Serializable;
+    
+import com.baomidou.mybatisplus.enums.IdType;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
-import com.baomidou.mybatisplus.annotations.TableName;
-import com.baomidou.mybatisplus.enums.IdType;
-import com.tjlou.mybatis.base.entity.IEntity;
-
-import java.io.Serializable;
-import java.util.Date;
 
 /**
  * <p>
@@ -18,135 +22,154 @@ import java.util.Date;
  * @since 2020-01-06
  */
 @TableName("task_cycle_info")
-public class TaskCycleInfo implements Serializable, IEntity {
+public class TaskCycleInfo implements Serializable,IEntity{
 
 
-    /**
+                /**
      * 主键
      */
-    @TableId(value = "id", type = IdType.AUTO)
-    private Long id;
-    /**
+        @TableId(value = "id", type = IdType.AUTO)
+        private Long id;
+            /**
      * 任务标识
      */
-    @TableField("task_dev_id")
+                @TableField("task_dev_id")
     private Long taskDevId;
-    /**
+            /**
      * 被指派者
      */
-    @TableField("account_id")
+                @TableField("account_id")
     private Long accountId;
-    /**
+            /**
      * 开始时间
      */
-    @TableField("create_time")
-    private Date createTime;
-    /**
+                @TableField("start_time")
+    private Date startTime;
+            /**
      * 结束时间
      */
-    @TableField("end_time")
+                @TableField("end_time")
     private Date endTime;
-    /**
+            /**
      * 任务状态  00A-未完成 00B-开发中 00C-已完成 00D-已测试
      */
-    private String status;
+            private String status;
+            /**
+     * 创建时间
+     */
+                @TableField("create_time")
+    private Date createTime;
 
-
-    public Long getId() {
+        
+public Long getId(){
         return id;
-    }
+        }
 
-    public TaskCycleInfo setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public Long getTaskDevId() {
+        public TaskCycleInfo setId(Long id){
+                this.id = id;
+                return this;
+            }
+        
+public Long getTaskDevId(){
         return taskDevId;
-    }
+        }
 
-    public TaskCycleInfo setTaskDevId(Long taskDevId) {
-        this.taskDevId = taskDevId;
-        return this;
-    }
-
-    public Long getAccountId() {
+        public TaskCycleInfo setTaskDevId(Long taskDevId){
+                this.taskDevId = taskDevId;
+                return this;
+            }
+        
+public Long getAccountId(){
         return accountId;
-    }
+        }
 
-    public TaskCycleInfo setAccountId(Long accountId) {
-        this.accountId = accountId;
-        return this;
-    }
+        public TaskCycleInfo setAccountId(Long accountId){
+                this.accountId = accountId;
+                return this;
+            }
+        
+public Date getStartTime(){
+        return startTime;
+        }
 
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public TaskCycleInfo setCreateTime(Date createTime) {
-        this.createTime = createTime;
-        return this;
-    }
-
-    public Date getEndTime() {
+        public TaskCycleInfo setStartTime(Date startTime){
+                this.startTime = startTime;
+                return this;
+            }
+        
+public Date getEndTime(){
         return endTime;
-    }
+        }
 
-    public TaskCycleInfo setEndTime(Date endTime) {
-        this.endTime = endTime;
-        return this;
-    }
-
-    public String getStatus() {
+        public TaskCycleInfo setEndTime(Date endTime){
+                this.endTime = endTime;
+                return this;
+            }
+        
+public String getStatus(){
         return status;
-    }
+        }
 
-    public TaskCycleInfo setStatus(String status) {
-        if (status != null)
-            status = status.trim();
-        this.status = status;
-        return this;
-    }
+        public TaskCycleInfo setStatus(String status){
+                    if(status != null)
+        status = status.trim();
+            this.status = status;
+                return this;
+            }
+        
+public Date getCreateTime(){
+        return createTime;
+        }
 
-    /**
-     * 表名
-     */
-    public static final String TABLE_NAME = "task_cycle_info";
-    /**
-     * 主键
-     */
-    public static final String ID = "id";
+        public TaskCycleInfo setCreateTime(Date createTime){
+                this.createTime = createTime;
+                return this;
+            }
 
-    /**
-     * 任务标识
-     */
-    public static final String TASK_DEV_ID = "task_dev_id";
+/**
+ * 表名
+ */
+public static final String TABLE_NAME="task_cycle_info" ;
+                    /**
+         * 主键
+         */
+            public static final String ID ="id" ;
 
-    /**
-     * 被指派者
-     */
-    public static final String ACCOUNT_ID = "account_id";
+                    /**
+         * 任务标识
+         */
+            public static final String TASK_DEV_ID ="task_dev_id" ;
 
-    /**
-     * 开始时间
-     */
-    public static final String CREATE_TIME = "create_time";
+                    /**
+         * 被指派者
+         */
+            public static final String ACCOUNT_ID ="account_id" ;
 
-    /**
-     * 结束时间
-     */
-    public static final String END_TIME = "end_time";
+                    /**
+         * 开始时间
+         */
+            public static final String START_TIME ="start_time" ;
 
-    /**
-     * 任务状态  00A-未完成 00B-开发中 00C-已完成 00D-已测试
-     */
-    public static final String STATUS = "status";
+                    /**
+         * 结束时间
+         */
+            public static final String END_TIME ="end_time" ;
 
+                    /**
+         * 任务状态  00A-未完成 00B-开发中 00C-已完成 00D-已测试
+         */
+            public static final String STATUS ="status" ;
 
-    @Override
-    public TaskCycleInfo pkVal(Serializable val) {
-        this.id = Long.valueOf(val.toString());
-        return this;
-    }
-}
+                    /**
+         * 创建时间
+         */
+            public static final String CREATE_TIME ="create_time" ;
+
+    
+@Override
+public TaskCycleInfo pkVal(Serializable val){
+                this.id = Long.valueOf(val.toString());
+            return this;
+        }
+        }
 
--- src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/TaskDevInfo.java
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/TaskDevInfo.java
@@ -1,13 +1,17 @@
 package com.tjlou.mybatis.auto.mysql.sps.entity;
 
+            import com.baomidou.mybatisplus.enums.IdType;
+        import java.util.Date;
+        import com.baomidou.mybatisplus.annotations.TableId;
+        import com.baomidou.mybatisplus.annotations.TableField;
+        import com.baomidou.mybatisplus.annotations.TableId;
+        import com.baomidou.mybatisplus.enums.IdType;
+        import com.baomidou.mybatisplus.annotations.TableName;
+        import java.io.Serializable;
+    
+import com.baomidou.mybatisplus.enums.IdType;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
-import com.baomidou.mybatisplus.annotations.TableName;
-import com.baomidou.mybatisplus.enums.IdType;
-import com.tjlou.mybatis.base.entity.IEntity;
-
-import java.io.Serializable;
-import java.util.Date;
 
 /**
  * <p>
@@ -18,251 +22,270 @@ import java.util.Date;
  * @since 2020-01-06
  */
 @TableName("task_dev_info")
-public class TaskDevInfo implements Serializable, IEntity {
+public class TaskDevInfo implements Serializable,IEntity{
 
 
-    /**
+                /**
      * 主键
      */
-    @TableId(value = "id", type = IdType.AUTO)
-    private Long id;
-    /**
+        @TableId(value = "id", type = IdType.AUTO)
+        private Long id;
+            /**
      * 标题
      */
-    private String title;
-    /**
+            private String title;
+            /**
      * 创建者
      */
-    @TableField("operator_id")
+                @TableField("operator_id")
     private Long operatorId;
-    /**
+            /**
      * 所属项目
      */
-    private String project;
-    /**
+            private String project;
+            /**
      * 模块
      */
-    private String module;
-    /**
+            private String module;
+            /**
      * 任务类型
      */
-    @TableField("task_type")
+                @TableField("task_type")
     private Integer taskType;
-    /**
+            /**
      * 内容
      */
-    private String content;
-    /**
+            private String content;
+            /**
      * 优先级
      */
-    private Integer priority;
-    /**
+            private Integer priority;
+            /**
      * 开始时间
      */
-    @TableField("create_time")
-    private Date createTime;
-    /**
+                @TableField("start_time")
+    private Date startTime;
+            /**
      * 结束时间
      */
-    @TableField("end_time")
+                @TableField("end_time")
     private Date endTime;
-    /**
+            /**
      * 预期工期
      */
-    private Integer expect;
-    /**
+            private Integer expect;
+            /**
      * 任务状态
      */
-    private String status;
+            private String status;
+            /**
+     * 创建时间
+     */
+                @TableField("create_time")
+    private Date createTime;
 
-
-    public Long getId() {
+        
+public Long getId(){
         return id;
-    }
+        }
 
-    public TaskDevInfo setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getTitle() {
+        public TaskDevInfo setId(Long id){
+                this.id = id;
+                return this;
+            }
+        
+public String getTitle(){
         return title;
-    }
+        }
 
-    public TaskDevInfo setTitle(String title) {
-        if (title != null)
-            title = title.trim();
-        this.title = title;
-        return this;
-    }
-
-    public Long getOperatorId() {
+        public TaskDevInfo setTitle(String title){
+                    if(title != null)
+        title = title.trim();
+            this.title = title;
+                return this;
+            }
+        
+public Long getOperatorId(){
         return operatorId;
-    }
+        }
 
-    public TaskDevInfo setOperatorId(Long operatorId) {
-        this.operatorId = operatorId;
-        return this;
-    }
-
-    public String getProject() {
+        public TaskDevInfo setOperatorId(Long operatorId){
+                this.operatorId = operatorId;
+                return this;
+            }
+        
+public String getProject(){
         return project;
-    }
+        }
 
-    public TaskDevInfo setProject(String project) {
-        if (project != null)
-            project = project.trim();
-        this.project = project;
-        return this;
-    }
-
-    public String getModule() {
+        public TaskDevInfo setProject(String project){
+                    if(project != null)
+        project = project.trim();
+            this.project = project;
+                return this;
+            }
+        
+public String getModule(){
         return module;
-    }
+        }
 
-    public TaskDevInfo setModule(String module) {
-        if (module != null)
-            module = module.trim();
-        this.module = module;
-        return this;
-    }
-
-    public Integer getTaskType() {
+        public TaskDevInfo setModule(String module){
+                    if(module != null)
+        module = module.trim();
+            this.module = module;
+                return this;
+            }
+        
+public Integer getTaskType(){
         return taskType;
-    }
+        }
 
-    public TaskDevInfo setTaskType(Integer taskType) {
-        this.taskType = taskType;
-        return this;
-    }
-
-    public String getContent() {
+        public TaskDevInfo setTaskType(Integer taskType){
+                this.taskType = taskType;
+                return this;
+            }
+        
+public String getContent(){
         return content;
-    }
+        }
 
-    public TaskDevInfo setContent(String content) {
-        if (content != null)
-            content = content.trim();
-        this.content = content;
-        return this;
-    }
-
-    public Integer getPriority() {
+        public TaskDevInfo setContent(String content){
+                    if(content != null)
+        content = content.trim();
+            this.content = content;
+                return this;
+            }
+        
+public Integer getPriority(){
         return priority;
-    }
+        }
 
-    public TaskDevInfo setPriority(Integer priority) {
-        this.priority = priority;
-        return this;
-    }
+        public TaskDevInfo setPriority(Integer priority){
+                this.priority = priority;
+                return this;
+            }
+        
+public Date getStartTime(){
+        return startTime;
+        }
 
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public TaskDevInfo setCreateTime(Date createTime) {
-        this.createTime = createTime;
-        return this;
-    }
-
-    public Date getEndTime() {
+        public TaskDevInfo setStartTime(Date startTime){
+                this.startTime = startTime;
+                return this;
+            }
+        
+public Date getEndTime(){
         return endTime;
-    }
+        }
 
-    public TaskDevInfo setEndTime(Date endTime) {
-        this.endTime = endTime;
-        return this;
-    }
-
-    public Integer getExpect() {
+        public TaskDevInfo setEndTime(Date endTime){
+                this.endTime = endTime;
+                return this;
+            }
+        
+public Integer getExpect(){
         return expect;
-    }
+        }
 
-    public TaskDevInfo setExpect(Integer expect) {
-        this.expect = expect;
-        return this;
-    }
-
-    public String getStatus() {
+        public TaskDevInfo setExpect(Integer expect){
+                this.expect = expect;
+                return this;
+            }
+        
+public String getStatus(){
         return status;
-    }
+        }
 
-    public TaskDevInfo setStatus(String status) {
-        if (status != null)
-            status = status.trim();
-        this.status = status;
-        return this;
-    }
+        public TaskDevInfo setStatus(String status){
+                    if(status != null)
+        status = status.trim();
+            this.status = status;
+                return this;
+            }
+        
+public Date getCreateTime(){
+        return createTime;
+        }
 
-    /**
-     * 表名
-     */
-    public static final String TABLE_NAME = "task_dev_info";
-    /**
-     * 主键
-     */
-    public static final String ID = "id";
+        public TaskDevInfo setCreateTime(Date createTime){
+                this.createTime = createTime;
+                return this;
+            }
 
-    /**
-     * 标题
-     */
-    public static final String TITLE = "title";
+/**
+ * 表名
+ */
+public static final String TABLE_NAME="task_dev_info" ;
+                    /**
+         * 主键
+         */
+            public static final String ID ="id" ;
 
-    /**
-     * 创建者
-     */
-    public static final String OPERATOR_ID = "operator_id";
+                    /**
+         * 标题
+         */
+            public static final String TITLE ="title" ;
 
-    /**
-     * 所属项目
-     */
-    public static final String PROJECT = "project";
+                    /**
+         * 创建者
+         */
+            public static final String OPERATOR_ID ="operator_id" ;
 
-    /**
-     * 模块
-     */
-    public static final String MODULE = "module";
+                    /**
+         * 所属项目
+         */
+            public static final String PROJECT ="project" ;
 
-    /**
-     * 任务类型
-     */
-    public static final String TASK_TYPE = "task_type";
+                    /**
+         * 模块
+         */
+            public static final String MODULE ="module" ;
 
-    /**
-     * 内容
-     */
-    public static final String CONTENT = "content";
+                    /**
+         * 任务类型
+         */
+            public static final String TASK_TYPE ="task_type" ;
 
-    /**
-     * 优先级
-     */
-    public static final String PRIORITY = "priority";
+                    /**
+         * 内容
+         */
+            public static final String CONTENT ="content" ;
 
-    /**
-     * 开始时间
-     */
-    public static final String CREATE_TIME = "create_time";
+                    /**
+         * 优先级
+         */
+            public static final String PRIORITY ="priority" ;
 
-    /**
-     * 结束时间
-     */
-    public static final String END_TIME = "end_time";
+                    /**
+         * 开始时间
+         */
+            public static final String START_TIME ="start_time" ;
 
-    /**
-     * 预期工期
-     */
-    public static final String EXPECT = "expect";
+                    /**
+         * 结束时间
+         */
+            public static final String END_TIME ="end_time" ;
 
-    /**
-     * 任务状态
-     */
-    public static final String STATUS = "status";
+                    /**
+         * 预期工期
+         */
+            public static final String EXPECT ="expect" ;
 
+                    /**
+         * 任务状态
+         */
+            public static final String STATUS ="status" ;
 
-    @Override
-    public TaskDevInfo pkVal(Serializable val) {
-        this.id = Long.valueOf(val.toString());
-        return this;
-    }
-}
+                    /**
+         * 创建时间
+         */
+            public static final String CREATE_TIME ="create_time" ;
+
+    
+@Override
+public TaskDevInfo pkVal(Serializable val){
+                this.id = Long.valueOf(val.toString());
+            return this;
+        }
+        }
 
