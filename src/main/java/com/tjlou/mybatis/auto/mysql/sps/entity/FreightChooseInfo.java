5d4095d336f39a357541d240accd8f1ae2a23ca3
wengzhongjie <534139685@qq.com>
2020-01-20 15:21:30 +0800

    #Mod sync 运费选择信息表

---
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/FreightChooseInfo.java
@@ -0,0 +1,106 @@
+package com.tjlou.mybatis.auto.mysql.sps.entity;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+import com.tjlou.mybatis.base.entity.IEntity;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 运费选择信息表
+ * </p>
+ *
+ * @author Gaby
+ * @since 2020-01-20
+ */
+@TableName("freight_choose_info")
+public class FreightChooseInfo implements Serializable, IEntity {
+
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    /**
+     * 店铺ID
+     */
+    @TableField("shop_uri")
+    private String shopUri;
+    /**
+     * 1-普通快递 2-顺丰快递 4-顺丰到付
+     */
+    private Integer type;
+    /**
+     * 是否显示
+     */
+    private Integer show;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public FreightChooseInfo setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getShopUri() {
+        return shopUri;
+    }
+
+    public FreightChooseInfo setShopUri(String shopUri) {
+        if (shopUri != null)
+            shopUri = shopUri.trim();
+        this.shopUri = shopUri;
+        return this;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public FreightChooseInfo setType(Integer type) {
+        this.type = type;
+        return this;
+    }
+
+    public Integer getShow() {
+        return show;
+    }
+
+    public FreightChooseInfo setShow(Integer show) {
+        this.show = show;
+        return this;
+    }
+
+    /**
+     * 表名
+     */
+    public static final String TABLE_NAME = "freight_choose_info";
+    public static final String ID = "id";
+
+    /**
+     * 店铺ID
+     */
+    public static final String SHOP_URI = "shop_uri";
+
+    /**
+     * 1-普通快递 2-顺丰快递 4-顺丰到付
+     */
+    public static final String TYPE = "type";
+
+    /**
+     * 是否显示
+     */
+    public static final String SHOW = "show";
+
+
+    @Override
+    public FreightChooseInfo pkVal(Serializable val) {
+        this.id = Long.valueOf(val.toString());
+        return this;
+    }
+}
+
---
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/service/impl/FreightChooseInfoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tjlou.mybatis.auto.mysql.sps.service.impl;
+
+import com.tjlou.mybatis.auto.mysql.sps.entity.FreightChooseInfo;
+import com.tjlou.mybatis.auto.mysql.sps.mapper.FreightChooseInfoMapper;
+import com.tjlou.mybatis.auto.mysql.sps.service.FreightChooseInfoService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 运费选择信息表 服务实现类
+ * </p>
+ *
+ * @author Gaby
+ * @since 2020-01-20
+ */
+@Service
+public class FreightChooseInfoServiceImpl extends ServiceImpl<FreightChooseInfoMapper, FreightChooseInfo> implements FreightChooseInfoService {
+
+}
