2ef2ee67a787f15ab3b66de2ac47e70e79a15ba3
wengzhongjie <534139685@qq.com>
2020-08-21 15:02:48 +0800

    #Mod  -syn 拍品置顶表

--- src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/ProductInstanceTopInfo.java
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/ProductInstanceTopInfo.java
@@ -1,17 +1,13 @@
 package com.tjlou.mybatis.auto.mysql.sps.entity;
 
-            import com.baomidou.mybatisplus.enums.IdType;
-        import java.util.Date;
-        import com.baomidou.mybatisplus.annotations.TableId;
-        import com.baomidou.mybatisplus.annotations.TableField;
-        import com.baomidou.mybatisplus.annotations.TableId;
-        import com.baomidou.mybatisplus.enums.IdType;
-        import com.baomidou.mybatisplus.annotations.TableName;
-        import java.io.Serializable;
-    
-import com.baomidou.mybatisplus.enums.IdType;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+import com.tjlou.mybatis.base.entity.IEntity;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * <p>
@@ -22,77 +18,77 @@ import com.baomidou.mybatisplus.annotations.TableId;
  * @since 2020-08-21
  */
 @TableName("product_instance_top_info")
-public class ProductInstanceTopInfo implements Serializable,IEntity{
+public class ProductInstanceTopInfo implements Serializable, IEntity {
 
 
-                /**
+    /**
      * 主键
      */
-        @TableId(value = "id", type = IdType.AUTO)
-        private Long id;
-            /**
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    /**
      * 所属拍品
      */
-                @TableField("product_instance_id")
+    @TableField("product_instance_id")
     private Long productInstanceId;
-            /**
+    /**
      * 创建时间
      */
-                @TableField("create_time")
+    @TableField("create_time")
     private Date createTime;
 
-        
-public Long getId(){
+
+    public Long getId() {
         return id;
-        }
+    }
 
-        public ProductInstanceTopInfo setId(Long id){
-                this.id = id;
-                return this;
-            }
-        
-public Long getProductInstanceId(){
+    public ProductInstanceTopInfo setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Long getProductInstanceId() {
         return productInstanceId;
-        }
+    }
 
-        public ProductInstanceTopInfo setProductInstanceId(Long productInstanceId){
-                this.productInstanceId = productInstanceId;
-                return this;
-            }
-        
-public Date getCreateTime(){
+    public ProductInstanceTopInfo setProductInstanceId(Long productInstanceId) {
+        this.productInstanceId = productInstanceId;
+        return this;
+    }
+
+    public Date getCreateTime() {
         return createTime;
-        }
+    }
 
-        public ProductInstanceTopInfo setCreateTime(Date createTime){
-                this.createTime = createTime;
-                return this;
-            }
+    public ProductInstanceTopInfo setCreateTime(Date createTime) {
+        this.createTime = createTime;
+        return this;
+    }
 
-/**
- * 表名
- */
-public static final String TABLE_NAME="product_instance_top_info" ;
-                    /**
-         * 主键
-         */
-            public static final String ID ="id" ;
+    /**
+     * 表名
+     */
+    public static final String TABLE_NAME = "product_instance_top_info";
+    /**
+     * 主键
+     */
+    public static final String ID = "id";
 
-                    /**
-         * 所属拍品
-         */
-            public static final String PRODUCT_INSTANCE_ID ="product_instance_id" ;
+    /**
+     * 所属拍品
+     */
+    public static final String PRODUCT_INSTANCE_ID = "product_instance_id";
 
-                    /**
-         * 创建时间
-         */
-            public static final String CREATE_TIME ="create_time" ;
+    /**
+     * 创建时间
+     */
+    public static final String CREATE_TIME = "create_time";
 
-    
-@Override
-public ProductInstanceTopInfo pkVal(Serializable val){
-                this.id = Long.valueOf(val.toString());
-            return this;
-        }
-        }
+
+    @Override
+    public ProductInstanceTopInfo pkVal(Serializable val) {
+        this.id = Long.valueOf(val.toString());
+        return this;
+    }
+}
 
