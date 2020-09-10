8b894a391fdcaea4f215962cc7c1ba40f8ca952a
wengzhongjie <534139685@qq.com>
2020-09-10 22:27:51 +0800

    #Mod  -syn 公众号请求日志

---
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/mapper/WebRequestLogInfoMapper.java
@@ -0,0 +1,16 @@
+package    com.tjlou.mybatis.auto.mysql.sps.mapper;
+
+import com.tjlou.mybatis.auto.mysql.sps.entity.WebRequestLogInfo;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 请求日志信息表(公众号) Mapper 接口
+ * </p>
+ *
+ * @author Gaby
+ * @since 2020-09-10
+ */
+public interface WebRequestLogInfoMapper extends BaseMapper<WebRequestLogInfo> {
+
+}
---
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/service/impl/WebRequestLogInfoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tjlou.mybatis.auto.mysql.sps.service.impl;
+
+import com.tjlou.mybatis.auto.mysql.sps.entity.WebRequestLogInfo;
+import com.tjlou.mybatis.auto.mysql.sps.mapper.WebRequestLogInfoMapper;
+import com.tjlou.mybatis.auto.mysql.sps.service.WebRequestLogInfoService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 请求日志信息表(公众号) 服务实现类
+ * </p>
+ *
+ * @author Gaby
+ * @since 2020-09-10
+ */
+@Service
+public class WebRequestLogInfoServiceImpl extends ServiceImpl<WebRequestLogInfoMapper, WebRequestLogInfo> implements WebRequestLogInfoService {
+
+}
