7acb0d38f0bbaec928985ac8e4e49add73ad4b41
wengzhongjie <534139685@qq.com>
2020-12-21 14:03:12 +0800

    #Mod  用户微信授权信息表,用户token信息表 1

--- src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/UserTokenInfo.java
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/UserTokenInfo.java
@@ -1,16 +1,12 @@
 package com.tjlou.mybatis.auto.mysql.sps.entity;
 
-            import com.baomidou.mybatisplus.enums.IdType;
-        import com.baomidou.mybatisplus.annotations.TableId;
-        import com.baomidou.mybatisplus.annotations.TableField;
-        import com.baomidou.mybatisplus.annotations.TableId;
-        import com.baomidou.mybatisplus.enums.IdType;
-        import com.baomidou.mybatisplus.annotations.TableName;
-        import java.io.Serializable;
-    
-import com.baomidou.mybatisplus.enums.IdType;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+import com.tjlou.mybatis.base.entity.IEntity;
+
+import java.io.Serializable;
 
 /**
  * <p>
@@ -21,202 +17,202 @@ import com.baomidou.mybatisplus.annotations.TableId;
  * @since 2020-12-21
  */
 @TableName("user_token_info")
-public class UserTokenInfo implements Serializable,IEntity{
+public class UserTokenInfo implements Serializable, IEntity {
 
 
-                /**
+    /**
      * 主键
      */
-        @TableId(value = "id", type = IdType.AUTO)
-        private Long id;
-            /**
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    /**
      * 所属用户
      */
-                @TableField("user_account_id")
+    @TableField("user_account_id")
     private Long userAccountId;
-            /**
+    /**
      * 移动端token
      */
-            private String token;
-            /**
+    private String token;
+    /**
      * 网页token
      */
-                @TableField("h5_token")
+    @TableField("h5_token")
     private String h5Token;
-            /**
+    /**
      * 小程序token
      */
-                @TableField("mp_token")
+    @TableField("mp_token")
     private String mpToken;
-            /**
+    /**
      * 公众号token
      */
-                @TableField("web_token")
+    @TableField("web_token")
     private String webToken;
-            /**
+    /**
      * 工具token
      */
-                @TableField("tool_token")
+    @TableField("tool_token")
     private String toolToken;
-            /**
+    /**
      * 嵌套token
      */
-                @TableField("nested_token")
+    @TableField("nested_token")
     private String nestedToken;
-            /**
+    /**
      * 系统类型  1-超级仓库 2-锤定 4-诸葛拍拍
      */
-                @TableField("system_type")
+    @TableField("system_type")
     private Integer systemType;
 
-        
-public Long getId(){
+
+    public Long getId() {
         return id;
-        }
+    }
 
-        public UserTokenInfo setId(Long id){
-                this.id = id;
-                return this;
-            }
-        
-public Long getUserAccountId(){
+    public UserTokenInfo setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Long getUserAccountId() {
         return userAccountId;
-        }
+    }
 
-        public UserTokenInfo setUserAccountId(Long userAccountId){
-                this.userAccountId = userAccountId;
-                return this;
-            }
-        
-public String getToken(){
+    public UserTokenInfo setUserAccountId(Long userAccountId) {
+        this.userAccountId = userAccountId;
+        return this;
+    }
+
+    public String getToken() {
         return token;
-        }
+    }
 
-        public UserTokenInfo setToken(String token){
-                    if(token != null)
-        token = token.trim();
-            this.token = token;
-                return this;
-            }
-        
-public String getH5Token(){
+    public UserTokenInfo setToken(String token) {
+        if (token != null)
+            token = token.trim();
+        this.token = token;
+        return this;
+    }
+
+    public String getH5Token() {
         return h5Token;
-        }
+    }
 
-        public UserTokenInfo setH5Token(String h5Token){
-                    if(h5Token != null)
-        h5Token = h5Token.trim();
-            this.h5Token = h5Token;
-                return this;
-            }
-        
-public String getMpToken(){
+    public UserTokenInfo setH5Token(String h5Token) {
+        if (h5Token != null)
+            h5Token = h5Token.trim();
+        this.h5Token = h5Token;
+        return this;
+    }
+
+    public String getMpToken() {
         return mpToken;
-        }
+    }
 
-        public UserTokenInfo setMpToken(String mpToken){
-                    if(mpToken != null)
-        mpToken = mpToken.trim();
-            this.mpToken = mpToken;
-                return this;
-            }
-        
-public String getWebToken(){
+    public UserTokenInfo setMpToken(String mpToken) {
+        if (mpToken != null)
+            mpToken = mpToken.trim();
+        this.mpToken = mpToken;
+        return this;
+    }
+
+    public String getWebToken() {
         return webToken;
-        }
+    }
 
-        public UserTokenInfo setWebToken(String webToken){
-                    if(webToken != null)
-        webToken = webToken.trim();
-            this.webToken = webToken;
-                return this;
-            }
-        
-public String getToolToken(){
+    public UserTokenInfo setWebToken(String webToken) {
+        if (webToken != null)
+            webToken = webToken.trim();
+        this.webToken = webToken;
+        return this;
+    }
+
+    public String getToolToken() {
         return toolToken;
-        }
+    }
 
-        public UserTokenInfo setToolToken(String toolToken){
-                    if(toolToken != null)
-        toolToken = toolToken.trim();
-            this.toolToken = toolToken;
-                return this;
-            }
-        
-public String getNestedToken(){
+    public UserTokenInfo setToolToken(String toolToken) {
+        if (toolToken != null)
+            toolToken = toolToken.trim();
+        this.toolToken = toolToken;
+        return this;
+    }
+
+    public String getNestedToken() {
         return nestedToken;
-        }
+    }
 
-        public UserTokenInfo setNestedToken(String nestedToken){
-                    if(nestedToken != null)
-        nestedToken = nestedToken.trim();
-            this.nestedToken = nestedToken;
-                return this;
-            }
-        
-public Integer getSystemType(){
+    public UserTokenInfo setNestedToken(String nestedToken) {
+        if (nestedToken != null)
+            nestedToken = nestedToken.trim();
+        this.nestedToken = nestedToken;
+        return this;
+    }
+
+    public Integer getSystemType() {
         return systemType;
-        }
+    }
 
-        public UserTokenInfo setSystemType(Integer systemType){
-                this.systemType = systemType;
-                return this;
-            }
+    public UserTokenInfo setSystemType(Integer systemType) {
+        this.systemType = systemType;
+        return this;
+    }
 
-/**
- * 表名
- */
-public static final String TABLE_NAME="user_token_info" ;
-                    /**
-         * 主键
-         */
-            public static final String ID ="id" ;
+    /**
+     * 表名
+     */
+    public static final String TABLE_NAME = "user_token_info";
+    /**
+     * 主键
+     */
+    public static final String ID = "id";
 
-                    /**
-         * 所属用户
-         */
-            public static final String USER_ACCOUNT_ID ="user_account_id" ;
+    /**
+     * 所属用户
+     */
+    public static final String USER_ACCOUNT_ID = "user_account_id";
 
-                    /**
-         * 移动端token
-         */
-            public static final String TOKEN ="token" ;
+    /**
+     * 移动端token
+     */
+    public static final String TOKEN = "token";
 
-                    /**
-         * 网页token
-         */
-            public static final String H5_TOKEN ="h5_token" ;
+    /**
+     * 网页token
+     */
+    public static final String H5_TOKEN = "h5_token";
 
-                    /**
-         * 小程序token
-         */
-            public static final String MP_TOKEN ="mp_token" ;
+    /**
+     * 小程序token
+     */
+    public static final String MP_TOKEN = "mp_token";
 
-                    /**
-         * 公众号token
-         */
-            public static final String WEB_TOKEN ="web_token" ;
+    /**
+     * 公众号token
+     */
+    public static final String WEB_TOKEN = "web_token";
 
-                    /**
-         * 工具token
-         */
-            public static final String TOOL_TOKEN ="tool_token" ;
+    /**
+     * 工具token
+     */
+    public static final String TOOL_TOKEN = "tool_token";
 
-                    /**
-         * 嵌套token
-         */
-            public static final String NESTED_TOKEN ="nested_token" ;
+    /**
+     * 嵌套token
+     */
+    public static final String NESTED_TOKEN = "nested_token";
 
-                    /**
-         * 系统类型  1-超级仓库 2-锤定 4-诸葛拍拍
-         */
-            public static final String SYSTEM_TYPE ="system_type" ;
+    /**
+     * 系统类型  1-超级仓库 2-锤定 4-诸葛拍拍
+     */
+    public static final String SYSTEM_TYPE = "system_type";
 
-    
-@Override
-public UserTokenInfo pkVal(Serializable val){
-                this.id = Long.valueOf(val.toString());
-            return this;
-        }
-        }
+
+    @Override
+    public UserTokenInfo pkVal(Serializable val) {
+        this.id = Long.valueOf(val.toString());
+        return this;
+    }
+}
 
--- src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/UserWxGrantInfo.java
+++ src/main/java/com/tjlou/mybatis/auto/mysql/sps/entity/UserWxGrantInfo.java
@@ -1,16 +1,12 @@
 package com.tjlou.mybatis.auto.mysql.sps.entity;
 
-            import com.baomidou.mybatisplus.enums.IdType;
-        import com.baomidou.mybatisplus.annotations.TableId;
-        import com.baomidou.mybatisplus.annotations.TableField;
-        import com.baomidou.mybatisplus.annotations.TableId;
-        import com.baomidou.mybatisplus.enums.IdType;
-        import com.baomidou.mybatisplus.annotations.TableName;
-        import java.io.Serializable;
-    
-import com.baomidou.mybatisplus.enums.IdType;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+import com.tjlou.mybatis.base.entity.IEntity;
+
+import java.io.Serializable;
 
 /**
  * <p>
@@ -21,172 +17,172 @@ import com.baomidou.mybatisplus.annotations.TableId;
  * @since 2020-12-21
  */
 @TableName("user_wx_grant_info")
-public class UserWxGrantInfo implements Serializable,IEntity{
+public class UserWxGrantInfo implements Serializable, IEntity {
 
 
-                @TableId(value = "id", type = IdType.AUTO)
-        private Long id;
-            /**
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    /**
      * 所属用户
      */
-                @TableField("user_account_id")
+    @TableField("user_account_id")
     private Long userAccountId;
-            /**
+    /**
      * 授权unionid
      */
-            private String unionid;
-            /**
+    private String unionid;
+    /**
      * 小程序openId
      */
-                @TableField("open_id")
+    @TableField("open_id")
     private String openId;
-            /**
+    /**
      * 移动端openId
      */
-                @TableField("app_open_id")
+    @TableField("app_open_id")
     private String appOpenId;
-            /**
+    /**
      * 公众号openId
      */
-                @TableField("pub_open_id")
+    @TableField("pub_open_id")
     private String pubOpenId;
-            /**
+    /**
      * 是否关注公众号
      */
-            private Integer subscribe;
-            /**
+    private Integer subscribe;
+    /**
      * 系统类型 1-超级仓库 2-锤定 4-诸葛拍拍
      */
-                @TableField("system_type")
+    @TableField("system_type")
     private Integer systemType;
 
-        
-public Long getId(){
+
+    public Long getId() {
         return id;
-        }
+    }
 
-        public UserWxGrantInfo setId(Long id){
-                this.id = id;
-                return this;
-            }
-        
-public Long getUserAccountId(){
+    public UserWxGrantInfo setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Long getUserAccountId() {
         return userAccountId;
-        }
+    }
 
-        public UserWxGrantInfo setUserAccountId(Long userAccountId){
-                this.userAccountId = userAccountId;
-                return this;
-            }
-        
-public String getUnionid(){
+    public UserWxGrantInfo setUserAccountId(Long userAccountId) {
+        this.userAccountId = userAccountId;
+        return this;
+    }
+
+    public String getUnionid() {
         return unionid;
-        }
+    }
 
-        public UserWxGrantInfo setUnionid(String unionid){
-                    if(unionid != null)
-        unionid = unionid.trim();
-            this.unionid = unionid;
-                return this;
-            }
-        
-public String getOpenId(){
+    public UserWxGrantInfo setUnionid(String unionid) {
+        if (unionid != null)
+            unionid = unionid.trim();
+        this.unionid = unionid;
+        return this;
+    }
+
+    public String getOpenId() {
         return openId;
-        }
+    }
 
-        public UserWxGrantInfo setOpenId(String openId){
-                    if(openId != null)
-        openId = openId.trim();
-            this.openId = openId;
-                return this;
-            }
-        
-public String getAppOpenId(){
+    public UserWxGrantInfo setOpenId(String openId) {
+        if (openId != null)
+            openId = openId.trim();
+        this.openId = openId;
+        return this;
+    }
+
+    public String getAppOpenId() {
         return appOpenId;
-        }
+    }
 
-        public UserWxGrantInfo setAppOpenId(String appOpenId){
-                    if(appOpenId != null)
-        appOpenId = appOpenId.trim();
-            this.appOpenId = appOpenId;
-                return this;
-            }
-        
-public String getPubOpenId(){
+    public UserWxGrantInfo setAppOpenId(String appOpenId) {
+        if (appOpenId != null)
+            appOpenId = appOpenId.trim();
+        this.appOpenId = appOpenId;
+        return this;
+    }
+
+    public String getPubOpenId() {
         return pubOpenId;
-        }
+    }
 
-        public UserWxGrantInfo setPubOpenId(String pubOpenId){
-                    if(pubOpenId != null)
-        pubOpenId = pubOpenId.trim();
-            this.pubOpenId = pubOpenId;
-                return this;
-            }
-        
-public Integer getSubscribe(){
+    public UserWxGrantInfo setPubOpenId(String pubOpenId) {
+        if (pubOpenId != null)
+            pubOpenId = pubOpenId.trim();
+        this.pubOpenId = pubOpenId;
+        return this;
+    }
+
+    public Integer getSubscribe() {
         return subscribe;
-        }
+    }
 
-        public UserWxGrantInfo setSubscribe(Integer subscribe){
-                this.subscribe = subscribe;
-                return this;
-            }
-        
-public Integer getSystemType(){
+    public UserWxGrantInfo setSubscribe(Integer subscribe) {
+        this.subscribe = subscribe;
+        return this;
+    }
+
+    public Integer getSystemType() {
         return systemType;
-        }
+    }
 
-        public UserWxGrantInfo setSystemType(Integer systemType){
-                this.systemType = systemType;
-                return this;
-            }
+    public UserWxGrantInfo setSystemType(Integer systemType) {
+        this.systemType = systemType;
+        return this;
+    }
 
-/**
- * 表名
- */
-public static final String TABLE_NAME="user_wx_grant_info" ;
-                public static final String ID ="id" ;
+    /**
+     * 表名
+     */
+    public static final String TABLE_NAME = "user_wx_grant_info";
+    public static final String ID = "id";
 
-                    /**
-         * 所属用户
-         */
-            public static final String USER_ACCOUNT_ID ="user_account_id" ;
+    /**
+     * 所属用户
+     */
+    public static final String USER_ACCOUNT_ID = "user_account_id";
 
-                    /**
-         * 授权unionid
-         */
-            public static final String UNIONID ="unionid" ;
+    /**
+     * 授权unionid
+     */
+    public static final String UNIONID = "unionid";
 
-                    /**
-         * 小程序openId
-         */
-            public static final String OPEN_ID ="open_id" ;
+    /**
+     * 小程序openId
+     */
+    public static final String OPEN_ID = "open_id";
 
-                    /**
-         * 移动端openId
-         */
-            public static final String APP_OPEN_ID ="app_open_id" ;
+    /**
+     * 移动端openId
+     */
+    public static final String APP_OPEN_ID = "app_open_id";
 
-                    /**
-         * 公众号openId
-         */
-            public static final String PUB_OPEN_ID ="pub_open_id" ;
+    /**
+     * 公众号openId
+     */
+    public static final String PUB_OPEN_ID = "pub_open_id";
 
-                    /**
-         * 是否关注公众号
-         */
-            public static final String SUBSCRIBE ="subscribe" ;
+    /**
+     * 是否关注公众号
+     */
+    public static final String SUBSCRIBE = "subscribe";
 
-                    /**
-         * 系统类型 1-超级仓库 2-锤定 4-诸葛拍拍
-         */
-            public static final String SYSTEM_TYPE ="system_type" ;
+    /**
+     * 系统类型 1-超级仓库 2-锤定 4-诸葛拍拍
+     */
+    public static final String SYSTEM_TYPE = "system_type";
 
-    
-@Override
-public UserWxGrantInfo pkVal(Serializable val){
-                this.id = Long.valueOf(val.toString());
-            return this;
-        }
-        }
+
+    @Override
+    public UserWxGrantInfo pkVal(Serializable val) {
+        this.id = Long.valueOf(val.toString());
+        return this;
+    }
+}
 
